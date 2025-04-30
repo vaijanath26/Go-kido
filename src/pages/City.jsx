ea84e9b2bd5fd6ec971a52d401d3c156cd33929b
Vaijanath <vaijanath2605@gmail.com>
2025-04-30 12:36:05 +0530

    changes

--- src/pages/City.jsx
+++ src/pages/City.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const City = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Redirect to homepage as soon as the City page loads
+    navigate("/");
+  }, [navigate]);
+
   return (
     <div>
-      <h1> </h1>
+  
     </div>
-  )
-}
+  );
+};
 
-export default City
+export default City;
